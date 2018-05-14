663166c4368ecba1680e9c649aec0db31bae2a9f
Patrick Hadlaw <patsh5665@gmail.com>
2018-05-13 23:34:19 -0400

    TASK: implement server

--- README.md
+++ README.md
@@ -15,17 +15,29 @@
 
 ## Build instructions
 
+### Server setup
 ```
 $ git clone https://github.com/patrickhadlaw/patrickhadlaw.com
 $ cd patrickhadlaw.com/frontend
 $ npm install -g @angular/core
 $ npm install -g @angular/cli
 $ npm install
+$ ng build --prod
+$ cp -r dist/* ../com/
+$ cd ..
+$ go build patrick-hadlaw-server.go
 ```
+
+### Setup
+* To be able to use contact features a mail server will need to be hosted
+* The mail server should have an address of noreply@&lt;smtp-host&gt;
+* When running server `--mail` flag should be set to your smtp-host name --smtp-port should be set and --smtp should be set to your smtp server
+* Upon running server you will be prompted for password for contact@&lt;smtp-host&gt; as well as the target contact email address
+* Resume should be placed in project dir as RESUME.pdf
 
 ## Run instructions: 
 
 ```
-$ cd patrickhadlaw.com/frontend
-$ ng serve
+$ cd patrickhadlaw.com
+$ ./patrick-hadlaw-server [--help] [--mail=<smtp-host>] [--smtp=<smtp-server>] [--smtp-port=<smtp-port>] [--log=<log-file>] [--port=<port>]
 ```
---
+++ patrick-hadlaw-server.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/smtp"
+	"os"
+	"strconv"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func LogHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Dict is a generic dictionary
+type Dict map[string]interface{}
+
+// Exists checks if key exists in dictionary
+func (dict Dict) Exists(key string) bool {
+	_, ok := dict[key]
+	return ok
+}
+
+// ToDict converts json to dictionary
+func ToDict(str string) (Dict, error) {
+	var dict Dict
+	err := json.Unmarshal([]byte(str), &dict)
+	if err != nil {
+		return nil, err
+	}
+	return dict, nil
+}
+
+// RequestToJSON turns request body into dictionary
+func RequestToJSON(request *http.Request) (Dict, error) {
+	decoder := json.NewDecoder(request.Body)
+	var dict Dict
+	err := decoder.Decode(&dict)
+	if err != nil {
+		return nil, err
+	}
+	return dict, nil
+}
+
+func main() {
+	var smtpHost string
+	flag.StringVar(&smtpHost, "mail", "patrickhadlaw.com", "mail host address")
+	var smtpServer string
+	flag.StringVar(&smtpServer, "smtp", "localhost", "smtp server address")
+	var logfile string
+	flag.StringVar(&logfile, "log", "runtime.log", "name of log file")
+	var port int
+	flag.IntVar(&port, "port", 80, "port to run server on")
+	var smtpPort int
+	flag.IntVar(&smtpPort, "smtp-port", 587, "port for smtp requests")
+
+	file, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening log-file: %v", err)
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, file)
+
+	log.SetOutput(multiWriter)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Contact-email-account: ")
+	contactEmail, _ := reader.ReadString('\n')
+
+	fmt.Print("Mail-server-password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatal("Invalid password")
+	}
+	password := string(bytePassword)
+	fmt.Printf("\n\n")
+
+	serveMux := http.NewServeMux()
+	apiMux := http.NewServeMux()
+
+	serveMux.Handle("/", http.FileServer(http.Dir("./com")))
+
+	serveMux.Handle("/api/", http.StripPrefix("/api", apiMux))
+
+	apiMux.HandleFunc("/message/send", func(w http.ResponseWriter, r *http.Request) {
+		js, err := RequestToJSON(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			if js.Exists("name") && js.Exists("email") && js.Exists("message") {
+				if name, ok := js["name"].(string); ok {
+					if email, ok := js["email"].(string); ok {
+						if message, ok := js["message"].(string); ok {
+							auth := smtp.PlainAuth("", "contact@"+smtpHost, password, smtpServer)
+							if err != nil {
+								log.Printf("ERROR: failed to create smtp auth state")
+								w.WriteHeader(http.StatusInternalServerError)
+							}
+							msg := []byte("From: " + email + "\r\n" +
+								"Name: " + name + "\r\n" +
+								"Subject: patrickhadlaw.com Contact message\r\n" +
+								"\r\n" + message)
+
+							err = smtp.SendMail(smtpServer+":"+strconv.Itoa(smtpPort), auth, "contact@"+smtpHost, []string{contactEmail}, msg)
+							if err != nil {
+								log.Printf("ERROR: failed to send mail")
+								w.WriteHeader(http.StatusInternalServerError)
+							} else {
+								log.Printf("Sent message from: %s with email: %s", name, email)
+							}
+						} else {
+							w.WriteHeader(http.StatusBadRequest)
+						}
+					} else {
+						w.WriteHeader(http.StatusBadRequest)
+					}
+				} else {
+					w.WriteHeader(http.StatusBadRequest)
+				}
+			} else {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+		}
+	})
+
+	server := http.Server{
+		Addr:    ":" + strconv.Itoa(port),
+		Handler: LogHandler(serveMux),
+	}
+
+	log.Println("serving patrickhadlaw.com on port:", port)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to launch http server")
+	}
+}
